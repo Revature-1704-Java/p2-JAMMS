2b5001557fcca8f6b3686233987ab838a240c5a7
Matthew McCormick <matthew.o.mccormick@gmail.com>
2018-01-18 20:00:51 -0500

    Removed unused import

--- ebook-store/src/main/java/com/revature/controllers/BookServlet.java
+++ ebook-store/src/main/java/com/revature/controllers/BookServlet.java
@@ -2,8 +2,6 @@ package com.revature.controllers;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServlet;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
